0b92d1d280b3cf149f2b1844d2fa31d8d773190d
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-09-01 19:29:29 -0500

    Se instaló Express, se creó servidor básico y se agregó gitignore oficial

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("¡Hola, este es mi nuevo backend de Savia!");
+const express = require("express");
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("hola mundo");
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server runnning on port ${PORT}`)
+})
